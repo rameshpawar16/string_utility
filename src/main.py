81802aa29efa58f7b00850faa70aa1aa8cf7acb8
rameshpawa <your.email@example.com>
2025-11-27 15:42:20 +0530

    2nd

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from string_utils import reverse_string,to_upper,to_lower,count_vowels
+from src.string_utils import reverse_string,to_upper,to_lower,count_vowels
 
 if __name__ =='__main__':
     sample = 'Hello World'
